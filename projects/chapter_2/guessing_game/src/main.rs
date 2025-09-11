33cc341d4b7a6a05c7c30228b9c60595354cdf75
carlomuntwyler <carlomuntwyler@gmail.com>
2025-09-11 11:09:38 +0200

    added random nubmer guess

--- projects/chapter_2/guessing_game/src/main.rs
+++ projects/chapter_2/guessing_game/src/main.rs
@@ -3,6 +3,9 @@ use std::io;
 fn main() {
     println!("Guess the number!");
 
+    let random_number = rand::thread_rng().gen_range(1..=100);
+    println!("The random number is: {random_number}. ")
+    
     println!("Please input your guess.");
     let mut guess = String::new();
 
