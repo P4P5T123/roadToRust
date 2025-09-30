01390c3039366ff45a7668e08d6af0c02590f0ac
carlomuntwyler <carlomuntwyler@gmail.com>
2025-09-30 14:21:22 +0200

    even more fun

--- projects/chapter_3/fun_in_function/src/main.rs
+++ projects/chapter_3/fun_in_function/src/main.rs
@@ -1,7 +1,13 @@
 fn main() {
     println!("Hello, world!");
     print_labeled_measurement(5, 'h');
+
+    let x = increment(5);
+    println!("The value of x is: {x}");
 }
 fn print_labeled_measurement(value: i32, unit_label: char) {
     println!("The measurement is: {value}{unit_label}");
+}
+fn increment(x: i32) -> i32 {
+    x + 1
 }
