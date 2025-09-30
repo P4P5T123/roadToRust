509fba572bd798c77d6770923ebd4385a5244bdc
carlomuntwyler <carlomuntwyler@gmail.com>
2025-09-30 13:59:44 +0200

    more shit.

---
+++ projects/chapter_3/array_out_of_bounds/src/main.rs
@@ -0,0 +1,18 @@
+use std::io;
+
+fn main() {
+    let array = [1, 2, 3, 4, 5];
+    println!("Please enter an array index");
+    let mut index = String::new();
+
+    io::stdin()
+        .read_line(&mut index)
+        .expect("Failed to read index. ");
+
+    let index: usize = index.trim()
+        .parse()
+        .expect("Index entered was not a number. ");
+
+    let element = array[index];
+    println!("Your selected element of the array is {}", element);
+}
